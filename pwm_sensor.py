f2a219b001148ceefcd07abbc4a4817c7e005717
kevinolson07 <kevin.olson07@gmail.com>
2020-09-19 09:46:15 -0700

    added the ability to commit data to database

--- pwm_sensor.py
+++ pwm_sensor.py
@@ -1,15 +1,27 @@
 import time
 import RPi.GPIO as GPIO
+import MySQLdb
+import Adafruit_MAX31855.MAX31855 as MAX31855
 
 toggle = 31
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(toggle, GPIO.IN)
-
 level = 0
 counter =0
 LOW = time.time()
 time_HIGH = 0
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(toggle, GPIO.IN)
+mysql = MySQLdb.connect(host='localhost',user='root' ,passwd='root',db='serverroom')
+
+def log_data(period, duty):
+    cur =mysql.cursor()
+    val = (period, duty)
+    cur.execute("INSERT INTO OTS_controller (period, duty) VALUES (%s,%s)", val)
+    mysql.commit()
+
+def get_temp():
+    
+
 while 1:
     
     if (GPIO.input(toggle) == 1 and level == 0):
@@ -22,6 +34,7 @@ while 1:
         print("LOW:", LOW)
         print("PERIOD:",period)
         print("Duty Cycle:", duty)
+        log_data(period,duty)
     elif(GPIO.input(toggle) == 0 and level == 1):
         LOW = time.time()
         HIGH = LOW - HIGH
@@ -29,8 +42,4 @@ while 1:
         level = 0
         print("HIGH:", HIGH)
         period=0
-# if 
-#     seconds = time.time()
-#     time.sleep(1)
-#     print(seconds)
-#     x=x+1
+
